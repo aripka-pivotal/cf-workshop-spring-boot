7871e7a0216d61ea278988c57d1a587143bd91d6
aripka-pivotal <aripka@gopivotal.com>
2015-11-06 12:39:41 -0600

    build trigger

--- src/main/java/io/pivotal/cf/workshop/CfWorkshopSpringBootApplication.java
+++ src/main/java/io/pivotal/cf/workshop/CfWorkshopSpringBootApplication.java
@@ -3,6 +3,9 @@ package io.pivotal.cf.workshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//some comment to trigger a build
+
+
 @SpringBootApplication
 public class CfWorkshopSpringBootApplication {
 
